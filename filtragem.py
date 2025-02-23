9272b8810d6327663dea523780d7370afacd8eb6
Guilherme Cadete <110258437+kadetete@users.noreply.github.com>
2025-02-23 22:40:52 +0000

    Corrigindo Titulos

--- filtragem.py
+++ filtragem.py
@@ -3,8 +3,8 @@ import xml.etree.ElementTree as ET
 
 # Caminhos dos arquivos
 xml_file = "./Bases_originais/senadores.xml"
-csv_file = "./Bases_originais/2021.csv"
-csv_output = "./Bases_filtradas/2021_filtrado.csv"
+csv_file = "./Bases_originais/2019.csv"
+csv_output = "./Bases_filtradas/2019_filtrado.csv"
 
 # 1. Ler o XML e extrair os nomes dos senadores
 tree = ET.parse(xml_file)
@@ -25,48 +25,62 @@ for parlamentar in root.findall(".//Parlamentar"):
 
 # 2. Ler o CSV
 df = pd.read_csv(csv_file, encoding="utf-8", sep=";", dtype=str)
-df.columns = df.columns.str.strip() 
+df.columns = df.columns.str.strip()
+
+df.rename(columns={
+    "Tipo de Servi�o": "Tipo de Serviço",
+    "Multa/Remarca��o": "Multa/Remarcação",
+    "GRU / Diferen�a Tarif�ria": "GRU / Diferença Tarifária",
+    "Tx.de Embarque": "Taxa de Embarque",
+    "***Desconto Contratual 0,04%": "Desconto Contratual 0,04%",
+    "Data do Cancelamento/ Remarca��o": "Data do Cancelamento/Remarcação",
+    "GRU / UPGRADE de  \n0    Classe": "GRU / Upgrade de Classe",
+    "Valor do Ree...": "Valor do Reembolso",
+    "Custo Efetivo SF": "Custo Efetivo Senado Federal"
+}, inplace=True)
 
 # 3. Excluir linhas onde 'Tipo de serviço' é 'Seguro Viagem'
-df = df[df["Tipo de Servi�o"] != "Seguro Viagem"]
+df = df[df["Tipo de Serviço"] != "Seguro Viagem"]
 
 #4. Corrigir Valores
 
-# if not df["Unnamed: 9"].isnull():
-#     df["Tarifa"] = df["Tarifa"].fillna("").astype(str).str.strip() + df["Unnamed: 9"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 9"], inplace=True)
+# df["Tarifa"] = df["Tarifa"].fillna("").astype(str).str.strip() + df["Unnamed: 9"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 9"], inplace=True)
 
-# if not df["Unnamed: 11"].isnull():
-#     df["Tx.de Embarque"] = df["Tx.de Embarque"].fillna("").astype(str).str.strip() + df["Unnamed: 11"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 11"], inplace=True)
 
-# if not df["Unnamed: 13"].isnull():
-#     df["Multa/Remarca��o"] = df["Multa/Remarca��o"].fillna("").astype(str).str.strip() + df["Unnamed: 13"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 13"], inplace=True)
+# df["Taxa de Embarque"] = df["Taxa de Embarque"].fillna("").astype(str).str.strip() + df["Unnamed: 11"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 11"], inplace=True)
 
-# if not df["Unnamed: 15"].isnull():
-#     df["Total"] = df["Total"].fillna("").astype(str).str.strip() + df["Unnamed: 15"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 15"], inplace=True)
 
-# if not df["Unnamed: 19"].isnull():
-#     df["***Desconto Contratual 0,04%"] = df["***Desconto Contratual 0,04%"].fillna("").astype(str).str.strip() + df["Unnamed: 19"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 19", "Unnamed: 17"], inplace=True)
+# df["Multa/Remarcação"] = df["Multa/Remarcação"].fillna("").astype(str).str.strip() + df["Unnamed: 13"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 13"], inplace=True)
 
-# if not df["Unnamed: 21"].isnull():
-#     df["Total com Desconto"] = df["Total com Desconto"].fillna("").astype(str).str.strip() + df["Unnamed: 21"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 21", "Unnamed: 24"], inplace=True)
 
-# if not df["Unnamed: 26"].isnull():
-#     df["GRU / Diferen�a Tarif�ria"] = df["GRU / Diferen�a Tarif�ria"].fillna("").astype(str).str.strip() + df["Unnamed: 26"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 26"], inplace=True)
+# df["Total"] = df["Total"].fillna("").astype(str).str.strip() + df["Unnamed: 15"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 15"], inplace=True)
 
-# if not df["Unnamed: 28"].isnull():
-#     df["Valor do Reembolso"] = df["Valor do Reembolso"].fillna("").astype(str).str.strip() + df["Unnamed: 28"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 28"], inplace=True)
 
-# if not df["Unnamed: 30"].isnull():
-#     df["Custo Efetivo SF"] = df["Custo Efetivo SF"].fillna("").astype(str).str.strip() + df["Unnamed: 30"].fillna("").astype(str).str.strip()
-#     df.drop(columns=["Unnamed: 30", "Unnamed: 31"], inplace=True)
+# df["Desconto Contratual 0,04%"] = df["Desconto Contratual 0,04%"].fillna("").astype(str).str.strip() + df["Unnamed: 19"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 19", "Unnamed: 17"], inplace=True)
+
+
+# df["Total com Desconto"] = df["Total com Desconto"].fillna("").astype(str).str.strip() + df["Unnamed: 21"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 21", "Unnamed: 24"], inplace=True)
+
+
+# df["GRU / Diferença Tarifária"] = df["GRU / Diferença Tarifária"].fillna("").astype(str).str.strip() + df["Unnamed: 26"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 26"], inplace=True)
+
+
+# df["Valor do Reembolso"] = df["Valor do Reembolso"].fillna("").astype(str).str.strip() + df["Unnamed: 28"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 28"], inplace=True)
+
+
+# df["Custo Efetivo SF"] = df["Custo Efetivo Senado Federal"].fillna("").astype(str).str.strip() + df["Unnamed: 30"].fillna("").astype(str).str.strip()
+# df.drop(columns=["Unnamed: 30", "Unnamed: 31"], inplace=True)
+
+df["Data de volta"] = df["Unnamed: 7"].fillna("").astype(str).str.strip()
+df.drop(columns=["Unnamed: 7"], inplace=True)
 
 
 # 5. Filtrar o DataFrame para manter apenas os senadores
